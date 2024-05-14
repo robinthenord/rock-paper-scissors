f4aa127908b5d647ff5a8ed6bd765eca387dc8f7
Robin <168934937+robinthenord@users.noreply.github.com>
2024-05-14 21:50:04 +0200

    add prompt for player choice

--- javascript.js
+++ javascript.js
@@ -1,15 +1,36 @@
+//Logic to get computer choice of 'rock, 'paper' or 'scissors'
 function computerChoice() {
   let random = Math.floor(Math.random() * 3)
   if (random === 0) {
-    console.log('Rock');
+    console.log('Computer played rock');
     return 'Rock';
   } else if (random === 1) {
-    console.log('paper');
+    console.log('Computer played paper');
     return 'Paper';
   } else {
-    console.log('scissors');
+    console.log('Computer played scissors');
     return 'Scissors';
     }
 };
 
-let pc = computerChoice();
+let cc = computerChoice();
+
+//Get human choice
+function humanChoice(){
+  let choice = prompt('Rock, paper or scissors?');
+  if (choice.toLowerCase() == 'rock'){
+    console.log('You played rock');
+    return choice.toLowerCase()
+  } else if (choice.toLowerCase() == 'paper'){
+    console.log('You played paper');
+    return choice.toLowerCase()
+  } else if (choice.toLowerCase() == 'scissors'){
+    console.log('You played scissors');
+    return choice.toLowerCase()
+  } else {
+    console.log('Enter rock, paper or scissors')
+    return 'Enter rock, paper or scissors'
+  }
+}
+
+let hc = humanChoice();

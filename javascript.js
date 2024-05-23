b4623d6c6de9e78711f17073db1b33ff7195d470
Robin <168934937+robinthenord@users.noreply.github.com>
2024-05-23 18:06:20 +0200

    get flow state and finish it all before committing

--- javascript.js
+++ javascript.js
@@ -1,51 +1,101 @@
-//Logic to get computer choice of 'rock, 'paper' or 'scissors'
-function getComputerChoice() {
-  let random = Math.floor(Math.random() * 3);
-  if (random === 0) {
-    console.log("-Computer: Rock");
-    return "rock";
-  } else if (random === 1) {
-    console.log("-Computer: Paper");
-    return "paper";
-  } else {
-    console.log("-Computer: Scissors");
-    return "scissors";
+let plrScore = 0;
+let pcScore = 0;
+
+//Get computer choice
+function pcPick() {
+  let pick = ["rock", "paper", "scissors"];
+  return pick[Math.floor(Math.random() * pick.length)];
+}
+
+let plrChoice;
+let pcChoice;
+
+//Play single round, score increment, return winner
+function playRound(plrChoice, pcChoice) {
+  if (plrChoice === pcChoice) {
+    return "Tie! Both players picked " + plrChoice;
+  } else if (plrChoice === "rock") {
+    if (pcChoice === "scissors") {
+      plrScore++;
+      return "Win! Rock beats scissors";
+    } else if (pcChoice === "paper") {
+      pcScore++;
+      return "Lose! Paper beats rock";
+    }
+  } else if (plrChoice === "paper") {
+    if (pcChoice === "rock") {
+      plrScore++;
+      return "Win! Paper beats rock";
+    } else if (pcChoice === "scissors") {
+      pcScore++;
+      return "Lose! Scissors beats paper";
+    }
+  } else if (plrChoice === "scissors") {
+    if (pcChoice === "paper") {
+      plrScore++;
+      return "Win! Scissors beats paper";
+    } else if (pcChoice === "rock") {
+      pcScore++;
+      return "Lose! Rock beats scissors";
+    }
   }
 }
 
-//Get human choice
-function getHumanChoice() {
-  let choice = prompt("Rock, paper or scissors? (R, P, S)");
-  if (choice.toLowerCase() == "r" || choice.toLowerCase() == "rock") {
-    console.log("-You: Rock");
-    return "rock";
-  } else if (choice.toLowerCase() == "p" || choice.toLowerCase() == "paper") {
-    console.log("-You: Paper");
-    return "paper";
-  } else if (choice.toLowerCase() == "s" || choice.toLowerCase() == "scissors") {
-    console.log("-You: Scissors");
-    return "scissors";
-  } else {
-    console.log("Enter rock, paper or scissors");
-    return "Enter rock, paper or scissors";
+const roundText = document.querySelector("#roundText");
+const endText = document.querySelector("#endText");
+const plrPoints = document.querySelector("#plrPoints");
+const pcPoints = document.querySelector("#pcPoints");
+
+//Play first to 3, track score, show restart btn when a player wins
+function playGame(pick) {
+  pcChoice = pcPick();
+
+  roundText.textContent = playRound(pick, pcChoice);
+  plrPoints.textContent = "You: " + plrScore;
+  pcPoints.textContent = "AI: " + pcScore;
+
+  if (plrScore === 3) {
+    roundText.textContent = "";
+    endText.textContent = "Winner! Our AI overlord was no match for you!";
+    restartBtn.hidden = false;
+    rockBtn.disabled = true;
+    scissorsBtn.disabled = true;
+    paperBtn.disabled = true;
+  } else if (pcScore === 3) {
+    roundText.textContent = "";
+    endText.textContent = "Defeated! Maybe you need some practice?";
+    restartBtn.hidden = false;
+    rockBtn.disabled = true;
+    scissorsBtn.disabled = true;
+    paperBtn.disabled = true;
   }
 }
 
-//Logic to play single round
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log("Draw!");
-  } else if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")) {
-    console.log("Win!");
-    humanScore++;
-  } else if (
-    (humanChoice === "rock" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "scissors") ||
-    (humanChoice === "scissors" && computerChoice === "rock")) {
-    console.log("Lose!");
-    computerScore++;
-  }
-}
+//Restart game
+const restartBtn = document.querySelector("#restart");
+restartBtn.addEventListener("click", function () {
+  roundText.textcontent = "";
+  endText.textcontent = "";
+  restartBtn.hidden = true;
+  rockBtn.disabled = false;
+  scissorsBtn.disabled = false;
+  paperBtn.disabled = false;
+  plrScore = 0;
+  pcScore = 0;
+  plrPoints.textContent = "You: 0";
+  pcPoints.textContent = "AI: 0";
+});
+
+//Make buttons button
+const rockBtn = document.querySelector("#rBtn");
+rockBtn.addEventListener("click", function () {
+  playGame("rock");
+});
+const paperBtn = document.querySelector("#pBtn");
+paperBtn.addEventListener("click", function () {
+  playGame("paper");
+});
+const scissorsBtn = document.querySelector("#sBtn");
+scissorsBtn.addEventListener("click", function () {
+  playGame("scissors");
+});
